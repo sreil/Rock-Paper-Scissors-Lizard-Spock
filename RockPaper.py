c63726346365e4e0eb1fec2be69e688f6e05e3b3
sreil <seanpreilly@gmail.com>
2015-09-06 16:29:09 -0600

    add file

---
+++ RockPaper.py
@@ -0,0 +1,7 @@
+# Rock-paper-scissors-lizard-Spock (RPSLS) is a variant of 
+# Rock-paper-scissors that allows five choices. Each choice
+# wins against two other choices, loses against two other 
+#choices and ties against itself. Much of RPSLS's popularity
+# is that it has been featured in 3 episodes of the TV series
+# "The Big Bang Theory".
+
