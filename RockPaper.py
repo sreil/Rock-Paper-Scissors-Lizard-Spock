29d8629e529dfd5099ad4cb5290b9133b96927d8
sreil <seanpreilly@gmail.com>
2015-09-11 18:58:22 -0600

    add conversion

--- RockPaper.py
+++ RockPaper.py
@@ -12,6 +12,16 @@
 # spock beats scissors and rock
 
 def name_to_number(name):
+	if name == "rock":
+		return 0
+	elif name == "Spock":
+		return 1
+	elif name == "paper":
+		return 2
+	elif name == "lizard":
+		return 3
+	elif name == "scissors":
+		return 4
 
 
 def number_to_name(number):
